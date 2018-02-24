37001679a42587910a0dbc6cd0bc366f1fe480ef
VTam25 <vivienne.tam2020@gmail.com>
2018-02-24 17:34:18 -0500

    jbj

---
+++ s1.js
@@ -0,0 +1,23 @@
+
+var options= {
+
+	type:"basic",
+	title:"Make sure to take care of yourself!",
+	message:"It's time to meditate! Try some of the videos in our meditation section.",
+	icon:"notification.png"
+
+};
+
+
+
+
+
+
+chrome.notifications.create(options, callback);
+	console.log("Popup done!");
+
+
+
+function callback(){
+
+}
