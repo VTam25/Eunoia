93be56a8a9d3e958c555447b657568d7c2f08f83
VTam25 <vivienne.tam2020@gmail.com>
2018-02-24 17:36:19 -0500

    hkgh

--- s1.js
+++ s1.js
@@ -4,7 +4,7 @@ var options= {
 	type:"basic",
 	title:"Make sure to take care of yourself!",
 	message:"It's time to meditate! Try some of the videos in our meditation section.",
-	icon:"notification.png"
+	iconUrl:"notification.png"
 
 };
 
